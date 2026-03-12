a66738892fb9bfb9c6cf3ff5c3b8d631a64b2c5c
DESKTOP-C5T1MTL\user <174405242+Dyna-Rise@users.noreply.github.com>
2026-03-13 07:32:29 +0900

    Aの仮完成（ダメージ処理OK）

--- Assets/0_Main/1_AEnemy1/A_Scripts/Enemy2Controller.cs
+++ Assets/0_Main/1_AEnemy1/A_Scripts/Enemy2Controller.cs
@@ -6,6 +6,8 @@ public class Enemy2Controller : MonoBehaviour
 {
     GameObject player; //Player情報
 
+    [Header("体力・スピード")]
+    public int life = 3;
 
     [Header("投擲物・発射位置")]
     public GameObject throwObject;
@@ -18,6 +20,16 @@ public class Enemy2Controller : MonoBehaviour
 
     [Header("索敵範囲")]
     public float range = 5.0f;
+
+    [Header("ダメージ時間・ダメージ移動量")]
+    public float stunTime = 0.2f;
+    public float damageSpeed = 0.5f;
+
+    float damageTimer; //ダメージ時間を測るタイマー
+    bool isDamage; //ダメージフラグ
+
+    [Header("点滅対象")]
+    public GameObject enemyBody;
 
 
     //投擲コルーチン
@@ -32,9 +44,42 @@ public class Enemy2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(throwingCoroutine == null)
+        //ダメージ中なら減らす
+        if (damageTimer > 0)
+        {
+            damageTimer -= Time.deltaTime;
+
+            float val = Mathf.Sin(Time.time * 50);
+            if (val > 0) enemyBody.SetActive(true);
+            else enemyBody.SetActive(false);
+
+        }
+        else if (isDamage)
+        {
+            enemyBody.SetActive(true);
+            isDamage = false;
+        }
+
+        if (throwingCoroutine == null)
         {
             throwingCoroutine = StartCoroutine(ThrowingCol());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerAttack")
+        {
+            if (damageTimer <= 0 && !isDamage)
+            {
+                life--;
+                if (life <= 0)
+                {
+                    Destroy(gameObject);
+                }
+                damageTimer = stunTime;
+                isDamage = true;
+            }
         }
     }
 
